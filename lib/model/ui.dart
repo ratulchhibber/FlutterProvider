34e4550093d2ce08d6601c0ec0a06e054cec4552
Ratul Chhibber <ratulchhibber@gmail.com>
2022-03-26 17:24:33 +0530

    * Model- You have a ChangeNotifier- this would emit the changes to fontSize with notifyListeners, which is called when fontSize is set

---
+++ lib/model/ui.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class UI with ChangeNotifier {
+  double _fontSize = 0.5;
+
+  set fontSize(newValue) {
+    _fontSize = newValue;
+    notifyListeners();
+  }
+
+  double get fontSize => _fontSize * 30;
+
+  double get sliderFontSize => _fontSize;
+}
